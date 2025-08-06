3c8f407f6a984ce5ebcb03382149ae8b4b1aa52f
pwaver <william.e.butler@gmail.com>
2025-08-05 21:46:15 -0400

    PyTorch export to onnx float32.

--- nnunetv2/utilities/OnnxToTorchscript.py
+++ nnunetv2/utilities/OnnxToTorchscript.py
@@ -5,11 +5,27 @@ import onnx
 import dill
 from onnx2torch import convert
 
+from onnxconverter_common.float16 import convert_float_to_float32_model
+import onnx
+
+# Load the float16 ONNX model
+float16_model_path = "model_float16.onnx"
+float16_model = onnx.load(float16_model_path)
+
+# Convert the model to float32
+float32_model = convert_float_to_float32_model(float16_model)
+
+# Save the converted float32 model
+float32_model_path = "model_float32.onnx"
+onnx.save(float32_model, float32_model_path)
+
+print(f"Model converted and saved to {float32_model_path}")
+
 import os
-rootPath = '/mnt/SliskiDrive/AWI/AWIBuffer/' if os.name == 'posix' else '/Volumes/Crucial X8/AWI/Data/'
+rootPath = '/mnt/SliskiDrive/AWI/AWIBuffer/' if os.name == 'posix' else '/Volumes/X10Pro/AWIBuffer/NetModels/'
 
 # onnxPath = rootPath + "UMambaBot-plans_unet_edge8_epochs250_2d-DC_and_CE_loss-w-1-20-20.onnx"
-onnxPath = rootPath + "PlainConvUNet-nnUNetPlans_3d_fullres-default.onnx"
+onnxPath = rootPath + "PlainConvUNet-nnUNetPlans_2d-reduced3-lowdosesim-DC_and_CE_loss-w-1-20-20.onnx"
 
 modelPerOnnx = convert(onnxPath)
 
